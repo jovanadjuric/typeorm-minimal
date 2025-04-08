ebbe14933d5cd1a0f30d2cef1051a35ee1b3d776
jovanadjuric <mat.jovana@gmail.com>
2025-04-08 18:44:26 +0200

    feat: add pg datasource

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,15 +1,30 @@
+import 'dotenv/config'
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { Post } from "./entities/post.entity"
 import { TypeormLogger } from "./logger"
-import { PostSubscriber } from "./subscribers/post.subscriber"
 
-export const AppDataSource = new DataSource({
+export const SqliteDataSource = new DataSource({
   type: "sqlite",
   database: ":memory:",
   synchronize: true,
-  entities: [Post],
-  migrations: [],
-  subscribers: [PostSubscriber],
+  entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  subscribers: [__dirname + '/subscribers/*.subscriber{.ts,.js}'],
   logger: new TypeormLogger(false)
 })
+
+export const PostgresDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.PG_HOST,
+  port: +process.env.PG_PORT,
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DATABASE,
+  entities: [__dirname + '/entities/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  subscribers: [__dirname + '/subscribers/*.subscriber{.ts,.js}'],
+  synchronize: true,
+  maxQueryExecutionTime: 3000,
+  logger: new TypeormLogger(false)
+})
+
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
-import { AppDataSource } from './data-source';
+import { PostgresDataSource, SqliteDataSource } from './data-source';
 import { Post } from './entities/post.entity';
 
-AppDataSource.initialize().then(async () => {
-  const postRepository = AppDataSource.manager.getRepository(Post)
+SqliteDataSource.initialize().then(async () => {
+  const postRepository = SqliteDataSource.manager.getRepository(Post)
 
   const post = await postRepository.save(postRepository.create({ title: "Lorem Ipsum", description: "Lorem ispum dolor sit amet" }))
+  console.log('sqlite', post)
+
+}).catch(error => console.log(error));
+
+PostgresDataSource.initialize().then(async () => {
+  const postRepository = PostgresDataSource.manager.getRepository(Post)
+
+  const post = await postRepository.save(postRepository.create({ title: "Lorem Ipsum", description: "Lorem ispum dolor sit amet" }))
+  console.log('postgres', post)
+
+  await postRepository.delete({})
 }).catch(error => console.log(error));
